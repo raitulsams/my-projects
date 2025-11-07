bc917807d239ac481f3810f886c464035e6979e5
raitulsams <raitulsams202@gmail.com>
2025-11-07 03:27:13 +0100

    ----

--- firebase-begins/src/firebase/firebase.init.js
+++ firebase-begins/src/firebase/firebase.init.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCKFxD41Nyh1mPOeIDs01RKPLJRiR96GiM",
+    //apiKey: "AIzaSyCKFxD41Nyh1mPOeIDs01RKPLJRiR96GiM",
     authDomain: "mealbangla.firebaseapp.com",
     projectId: "mealbangla",
     storageBucket: "mealbangla.firebasestorage.app",
